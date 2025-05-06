752aabe54a81ea002c07874693a395adb09ef051
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2025-05-06 09:38:43 +0200

    Format .js and .css files using Rider

--- src/Steeltoe.io/wwwroot/js/main.js
+++ src/Steeltoe.io/wwwroot/js/main.js
@@ -1,39 +1,38 @@
 // modified copy of https://github.com/dotnet/docfx/blob/main/templates/modern/src/docfx.ts
 
 function setTheme(theme) {
-    localStorage.setItem("theme", theme)
-    if (theme === "auto") {
-        document.documentElement.setAttribute("data-bs-theme", window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-        document.getElementById("theme").className = "bi bi-circle-half";
+  localStorage.setItem("theme", theme)
+  if (theme === "auto") {
+    document.documentElement.setAttribute("data-bs-theme", window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+    document.getElementById("theme").className = "bi bi-circle-half";
+  } else {
+    document.documentElement.setAttribute("data-bs-theme", theme)
+    if (theme === "light") {
+      document.getElementById("theme").className = "bi bi-sun";
     } else {
-        document.documentElement.setAttribute("data-bs-theme", theme)
-        if (theme === "light") {
-          document.getElementById("theme").className = "bi bi-sun";
-        }
-        else {
-          document.getElementById("theme").className ="bi bi-moon";
-        }
+      document.getElementById("theme").className = "bi bi-moon";
     }
+  }
 }
 
 async function getDefaultTheme() {
-    return localStorage.getItem("theme") || "auto"
+  return localStorage.getItem("theme") || "auto"
 }
 
 async function initTheme() {
-    setTheme(await getDefaultTheme())
+  setTheme(await getDefaultTheme())
 }
 
 function onThemeChange(callback) {
-    return new MutationObserver(() => callback(getTheme()))
-        .observe(document.documentElement, { attributes: true, attributeFilter: ["data-bs-theme"] })
+  return new MutationObserver(() => callback(getTheme()))
+    .observe(document.documentElement, {attributes: true, attributeFilter: ["data-bs-theme"]})
 }
 
 function getTheme() {
-    return document.documentElement.getAttribute("data-bs-theme")
+  return document.documentElement.getAttribute("data-bs-theme")
 }
 
 function changeTheme(e, theme) {
-    e.preventDefault()
-    setTheme(theme)
+  e.preventDefault()
+  setTheme(theme)
 }
